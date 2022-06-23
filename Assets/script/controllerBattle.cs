a952e5f3a639940ee1af10938d8b71c87e4b1cb6
shiftsmart <shiftsmart@gmail.com>
2022-06-23 08:04:12 +0800

    22

--- Assets/script/controllerBattle.cs
+++ Assets/script/controllerBattle.cs
@@ -8,13 +8,14 @@ public class controllerBattle : controller
         base.PlayerCtl();
         if (Input.GetKeyDown(KeyCode.Z))
         {
+            if (!isGround) { body.AddForce(new Vector2(0, 65)); }
             body.velocity = new Vector2(0, body.velocity.y);
             anim.SetInteger("Attack", anim.GetInteger("Attack") + 1);
         }
 
     }
     public override void Move(int i) {
-        print("aaaaaaaaaa:"+ state.IsTag("lock"));
+       
         if (!CanMove())
         {
             body.velocity = new Vector2(i * speed * Time.deltaTime, body.velocity.y);
